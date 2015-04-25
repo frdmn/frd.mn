eb723c48b64372a2685408256e4149afd0300572
Jonas Friedmann <j@frd.mn>
2015-04-25 14:00:49 +0200

    Move bower_components/ into vendor/

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         dirs: {
-            bower: 'bower_components',
+            bower: 'vendor/bower_components',
             css: 'assets/css',
             js: 'assets/js',
             images: 'assets/images',
