051d6b688e509d4c2b24deeabcef5731db40a8b7
Jonas Friedmann <j@frd.mn>
2015-04-26 13:38:05 +0200

    Shorthand echo's in index.php

--- index.php
+++ index.php
@@ -14,21 +14,21 @@
           <div class="labeled-text__label-wrap">
             <span class="labeled-text__label">Name</span>
           </div>
-          <p class="typewriter"><?php echo $info['about']['name']; ?></p>
+          <p class="typewriter"><?= $info['about']['name']; ?></p>
         </div>
 
         <div class="labeled-text">
           <div class="labeled-text__label-wrap">
             <span class="labeled-text__label">City of Residence</span>
           </div>
-          <p class="typewriter"><?php echo $info['about']['city']; ?></p>
+          <p class="typewriter"><?= $info['about']['city']; ?></p>
         </div>
 
         <div class="labeled-text">
           <div class="labeled-text__label-wrap">
             <span class="labeled-text__label">Biography</span>
           </div>
-          <p class="typewriter"><?php echo $info['about']['bio']; ?></p>
+          <p class="typewriter"><?= $info['about']['bio']; ?></p>
         </div>
       </div><!--
    --><div class="grid__item width-lap--1of2">
@@ -43,13 +43,13 @@
                   <div class="labeled-text__label-wrap">
                     <span class="labeled-text__label">E-Mail</span>
                   </div>
-                  <p class="typewriter"><a href="<?php echo $info['contact']['mail']['link']; ?>"><?php echo $info['contact']['mail']['title']; ?></a>*</p>
+                  <p class="typewriter"><a href="<?= $info['contact']['mail']['link']; ?>"><?= $info['contact']['mail']['title']; ?></a>*</p>
                 </div>
                 <div class="labeled-text">
                   <div class="labeled-text__label-wrap">
                     <span class="labeled-text__label">Blog</span>
                   </div>
-                  <p class="typewriter"><a href="<?php echo $info['contact']['blog']['link']; ?>"><?php echo $info['contact']['blog']['title']; ?></a></p>
+                  <p class="typewriter"><a href="<?= $info['contact']['blog']['link']; ?>"><?= $info['contact']['blog']['title']; ?></a></p>
                 </div>
 
               </div><!--
@@ -58,13 +58,13 @@
                   <div class="labeled-text__label-wrap">
                     <span class="labeled-text__label">Twitter</span>
                   </div>
-                  <p class="typewriter"><a href="<?php echo $info['contact']['twitter']['link']; ?>"><?php echo $info['contact']['twitter']['title']; ?></a></p>
+                  <p class="typewriter"><a href="<?= $info['contact']['twitter']['link']; ?>"><?= $info['contact']['twitter']['title']; ?></a></p>
                 </div>
                 <div class="labeled-text">
                   <div class="labeled-text__label-wrap">
                     <span class="labeled-text__label">Keybase</span>
                   </div>
-                  <p class="typewriter"><a href="<?php echo $info['contact']['keybase']['link']; ?>"><?php echo $info['contact']['keybase']['title']; ?></a></p>
+                  <p class="typewriter"><a href="<?= $info['contact']['keybase']['link']; ?>"><?= $info['contact']['keybase']['title']; ?></a></p>
                 </div>
               </div>
             </div>
@@ -92,13 +92,13 @@
       <?php foreach ($projects as $project): ?>
         <div class="project-table__row">
           <div class="project-table__date">
-            <p class="typewriter space--bottom-none"><?php echo explode("-", $project['date'])[0]; ?><span class="typewriter__prefill">—</span><?php echo explode("-", $project['date'])[1]; ?><span class="typewriter__prefill">—</span><?php echo explode("-", $project['date'])[2]; ?></p>
+            <p class="typewriter space--bottom-none"><?= explode("-", $project['date'])[0]; ?><span class="typewriter__prefill">—</span><?= explode("-", $project['date'])[1]; ?><span class="typewriter__prefill">—</span><?= explode("-", $project['date'])[2]; ?></p>
           </div>
           <div class="project-table__title">
-            <p class="typewriter space--bottom-none"><a href="<?php echo $project['alias']; ?>.html"><?php echo $project['name']; ?></a></p>
+            <p class="typewriter space--bottom-none"><a href="<?= $project['alias']; ?>.html"><?= $project['name']; ?></a></p>
           </div>
           <div class="project-table__category">
-            <p class="typewriter space--bottom-none"><?php echo $project['category']; ?></p>
+            <p class="typewriter space--bottom-none"><?= $project['category']; ?></p>
           </div>
         </div>
       <?php endforeach; ?>
