d0b96bc1a4b9fae88042d013df434721219b2a95
Marian Friedmann <marian@ideenfrische.de>
2015-05-31 13:01:02 +0200

    Adjust gpg tooltip message

--- templates/pages/home.php
+++ templates/pages/home.php
@@ -25,7 +25,7 @@
             <a class="tooltip button button-- button--pill button--fade-right" href="<?= $data['info']['contact']['mail']['link']; ?>">
               <?= $data['info']['contact']['mail']['title']; ?>
               <span class="tooltip__content">
-                <span class="fake-link fake-link--block open-gpg">Fancy&nbsp;some&nbsp;privacy?</span>
+                <span class="fake-link fake-link--block open-gpg">Do&nbsp;you&nbsp;even&nbsp;GPG?</span>
               </span>
             </a>
           </li>
